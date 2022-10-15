12627b8d119ecda8859871725f915ee893e89872
Joyee <82671884+Ahmed-Joyee@users.noreply.github.com>
2022-10-15 22:35:15 +0600

    bootom_nav, fragments
    added bottom navigation bar and miscellaneous  changes in other layouts

--- app/src/main/java/app/android/audiophile/MainActivity.java
+++ app/src/main/java/app/android/audiophile/MainActivity.java
@@ -1,32 +1,16 @@
-package app.android.audiophile;
+import android.content.Intent;
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import com.example.q_mark.StartActivity.Welcome_page;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        goLoginActivity();
-    }
-    public void goLoginActivity(){
-        TextView bb = (TextView) findViewById(R.id.bb);
-
-        bb.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+        getSupportActionBar().hide();
+        startActivity(new Intent(MainActivity.this, Welcome_page.class));
     }
 }
