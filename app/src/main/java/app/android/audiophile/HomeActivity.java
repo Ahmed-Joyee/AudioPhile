562ecc19b865efd483bedcaee1b11931da127ae9
Bernini0 <tasnimbinanwar2605@gmail.com>
2022-10-17 22:27:07 +0600

    bottom navigation

--- app/src/main/java/app/android/audiophile/HomeActivity.java
+++ app/src/main/java/app/android/audiophile/HomeActivity.java
@@ -1,40 +1,50 @@
 package app.android.audiophile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private ChipNavigationBar chipNavigationBar;
+    private Fragment fragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        goOnlocal();
-        goOnVoice();
-    }
+        chipNavigationBar = findViewById(R.id.chipNavigation);
+        chipNavigationBar.setItemSelected(R.id.chat, true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new ChatFragment()).commit();
 
-    public void goOnlocal(){
-        Button goToLocal = findViewById(R.id.local_music);
-        goToLocal.setOnClickListener(new View.OnClickListener() {
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(HomeActivity.this,PlayerActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-    public void goOnVoice(){
-        Button goToVoice = findViewById(R.id.Voice);
-        goToVoice.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(HomeActivity.this,VoiceMessageActivity.class);
-                startActivity(intent);
+            public void onItemSelected(int i) {
+                switch (i){
+                    case R.id.chat:
+                        fragment = new ChatFragment();
+                        break;
+                    case R.id.profile:
+                        fragment = new ProfileFragment();
+                        break;
+                    case R.id.local:
+                        fragment = new LocalSongsFragment();
+                        break;
+                    case R.id.friend:
+                        fragment = new SearchFriendsFragment();
+                        break;
+                }
+
+                if(fragment != null){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+                }
             }
         });
     }
---
+++ app/src/main/java/app/android/audiophile/LocalSongsFragment.java
@@ -0,0 +1,148 @@
+package app.android.audiophile;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LocalSongsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LocalSongsFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+    RecyclerView recyclerView;
+    TextView noMusicTextView;
+    ArrayList<AudioModel> songsList = new ArrayList<>();
+
+    public LocalSongsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment LocalSongsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static LocalSongsFragment newInstance(String param1, String param2) {
+        LocalSongsFragment fragment = new LocalSongsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_local_songs, container, false);
+
+        recyclerView = view.findViewById(R.id.recycler1_view);
+        noMusicTextView = view.findViewById(R.id.no1_songs_text);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        if(checkPermission() == false){
+            requestPermission();
+//            return;
+        }
+
+        String[] projection = {
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.DURATION
+        };
+
+        String selection = MediaStore.Audio.Media.IS_MUSIC +" != 0";
+        Cursor cursor = view.getContext().getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,projection,selection,null,null);
+        while(cursor.moveToNext()){
+            Log.d("fragment", "son");
+            AudioModel songData = new AudioModel(cursor.getString(1),cursor.getString(0),cursor.getString(2));
+            if(new File(songData.getPath()).exists())
+                songsList.add(songData);
+        }
+
+        if(songsList.size()==0){
+            noMusicTextView.setVisibility(View.VISIBLE);
+        }else{
+            //recyclerview
+            recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+            recyclerView.setAdapter(new MusicListAdapter(songsList,view.getContext().getApplicationContext()));
+        }
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    boolean checkPermission(){
+        int result = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE);
+        if(result == PackageManager.PERMISSION_GRANTED){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    void requestPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),Manifest.permission.READ_EXTERNAL_STORAGE)){
+            Toast.makeText(getActivity(),"READ PERMISSION IS REQUIRED,PLEASE ALLOW FROM SETTTINGS",Toast.LENGTH_SHORT).show();
+        }else
+            ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},123);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(recyclerView!=null){
+            recyclerView.setAdapter(new MusicListAdapter(songsList,getActivity().getApplicationContext()));
+        }
+    }
+}
