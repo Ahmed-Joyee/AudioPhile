763b8c843aa0e951d89f10df33549a24ae33dbf7
Joyee <82671884+Ahmed-Joyee@users.noreply.github.com>
2022-11-01 00:42:28 +0600

    added swipe and drag gestures

---
+++ app/src/main/java/app/android/audiophile/MoveCallBack.java
@@ -0,0 +1,141 @@
+package app.android.audiophile;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+abstract public class MoveCallBack extends ItemTouchHelper.Callback {
+
+    private final ItemTouchHelperContract Adapter;
+
+    Context mContext;
+    private Paint mClearPaint;
+    private ColorDrawable mBackground;
+    private int backgroundColor;
+    private Drawable deleteDrawable;
+    private int intrinsicWidth;
+    private int intrinsicHeight;
+
+
+    MoveCallBack(ItemTouchHelperContract adapter, Context context) {
+        Adapter = adapter;
+        mContext = context;
+        mBackground = new ColorDrawable();
+        backgroundColor = Color.parseColor("#85586F");
+        mClearPaint = new Paint();
+        mClearPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+        deleteDrawable = ContextCompat.getDrawable(mContext, R.drawable.ic_delete);
+        intrinsicWidth = deleteDrawable.getIntrinsicWidth();
+        intrinsicHeight = deleteDrawable.getIntrinsicHeight();
+
+
+    }
+
+    public boolean isLongPressDragEnabled() {
+        return false;
+    }
+
+    @Override
+    public int getMovementFlags(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
+        int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
+        int swipeFlags = ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT;
+        return makeMovementFlags(dragFlags, swipeFlags);
+    }
+
+    @Override
+    public boolean onMove(@NonNull RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                          RecyclerView.ViewHolder target) {
+        Adapter.onRowMoved(viewHolder.getAdapterPosition(), target.getAdapterPosition());
+        return true;
+    }
+
+    @Override
+    public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+
+        View itemView = viewHolder.itemView;
+        int itemHeight = itemView.getHeight();
+
+        boolean isCancelled = dX == 0 && !isCurrentlyActive;
+
+        if (isCancelled) {
+            clearCanvas(c, itemView.getRight() + dX, (float) itemView.getTop(), (float) itemView.getRight(), (float) itemView.getBottom());
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            return;
+        }
+
+        mBackground.setColor(backgroundColor);
+        mBackground.setBounds(itemView.getRight() + (int) dX, itemView.getTop(), itemView.getRight(), itemView.getBottom());
+        mBackground.draw(c);
+
+        int deleteIconTop = itemView.getTop() + (itemHeight - intrinsicHeight) / 2;
+        int deleteIconMargin = (itemHeight - intrinsicHeight) / 2;
+        int deleteIconLeft = itemView.getRight() - deleteIconMargin - intrinsicWidth;
+        int deleteIconRight = itemView.getRight() - deleteIconMargin;
+        int deleteIconBottom = deleteIconTop + intrinsicHeight;
+
+
+        deleteDrawable.setBounds(deleteIconLeft, deleteIconTop, deleteIconRight, deleteIconBottom);
+        deleteDrawable.draw(c);
+
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+
+
+    }
+
+    private void clearCanvas(Canvas c, Float left, Float top, Float right, Float bottom) {
+        c.drawRect(left, top, right, bottom, mClearPaint);
+
+    }
+
+    @Override
+    public float getSwipeThreshold(@NonNull RecyclerView.ViewHolder viewHolder) {
+        return 0.7f;
+    }
+
+    public void onSelectedChanged(RecyclerView.ViewHolder viewHolder,
+                                  int actionState) {
+
+
+        if (actionState != ItemTouchHelper.ACTION_STATE_IDLE) {
+            if (viewHolder instanceof RVAdapter.MyViewHolder) {
+                RVAdapter.MyViewHolder myViewHolder=
+                        (RVAdapter.MyViewHolder) viewHolder;
+                Adapter.onRowSelected(myViewHolder);
+            }
+
+        }
+
+        super.onSelectedChanged(viewHolder, actionState);
+    }
+    @Override
+    public void clearView(@NonNull RecyclerView recyclerView,
+                          @NonNull RecyclerView.ViewHolder viewHolder) {
+        super.clearView(recyclerView, viewHolder);
+
+        if (viewHolder instanceof RVAdapter.MyViewHolder) {
+            RVAdapter.MyViewHolder myViewHolder=
+                    (RVAdapter.MyViewHolder) viewHolder;
+            Adapter.onRowClear(myViewHolder);
+        }
+    }
+
+    public interface ItemTouchHelperContract {
+
+        void onRowMoved(int fromPosition, int toPosition);
+        void onRowSelected(RVAdapter.MyViewHolder myViewHolder);
+        void onRowClear(RVAdapter.MyViewHolder myViewHolder);
+
+    }
+}
---
+++ app/src/main/java/app/android/audiophile/RVAdapter.java
@@ -0,0 +1,78 @@
+package app.android.audiophile;
+
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.MyViewHolder> implements MoveCallBack.ItemTouchHelperContract {
+
+    private ArrayList<String> data;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView mTitle;
+        View row;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            row = itemView;
+            mTitle = itemView.findViewById(R.id.music_title_text);
+        }
+    }
+
+    public RVAdapter(ArrayList<String> data) {
+        this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_item, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, int position) {
+        holder.mTitle.setText(data.get(position));
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+
+    @Override
+    public void onRowMoved(int fromPosition, int toPosition) {
+        if (fromPosition < toPosition) {
+            for (int i = fromPosition; i < toPosition; i++) {
+                Collections.swap(data, i, i + 1);
+            }
+        } else {
+            for (int i = fromPosition; i > toPosition; i--) {
+                Collections.swap(data, i, i - 1);
+            }
+        }
+        notifyItemMoved(fromPosition, toPosition);
+    }
+
+    @Override
+    public void onRowSelected(MyViewHolder myViewHolder) {
+        myViewHolder.row.setBackgroundColor(Color.rgb(255,148,148));
+    }
+
+    @Override
+    public void onRowClear(MyViewHolder myViewHolder) {
+        myViewHolder.row.setBackgroundColor(Color.rgb(255,178,178));
+    }
+}
